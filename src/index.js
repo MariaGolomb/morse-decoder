358361d017861a172f4410a7478efcf5c08366f7
MariaGolomb <51956868+MariaGolomb@users.noreply.github.com>
2019-10-04 23:18:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,6 +39,39 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+     let first=0;
+    let last=10;
+    let res='';
+    while (last<=expr.length){
+        let letter=expr.slice(first, last);
+        first+=10;
+        last+=10;
+        if(letter==='**********'){
+             res+=' ';
+        } else {
+            let resRevLetter='';
+            let i=letter.length;
+            let smb=letter.slice(i-2, i);
+            while (smb!=="00" && i>0){
+                if(smb==='10'){
+                    smb='.'
+                } else if(smb==='11'){
+                    smb='-'
+                }
+                resRevLetter+=smb;
+                i-=2;
+                smb=letter.slice(i-2, i);
+            }
+
+            res+=MORSE_TABLE[resRevLetter.split('').reverse().join('')];
+            
+        }
+
+    }
+
+
+    return res;
+
 }
 
 module.exports = {
